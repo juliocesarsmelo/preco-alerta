3ff8293c74802470f7ed5cfc657599664d731411
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2026-04-21 22:57:24 -0300

    alterando gitigore e adicionando arquivo database.php

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$database = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
