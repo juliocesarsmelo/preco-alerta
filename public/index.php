871c6b64eb72fd933554450cb3c7413460b58934
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2026-04-21 21:59:01 -0300

    estrutura inicial

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preço Alerta</title>
+</head>
+<body>
+    <h1>Preço Alerta</h1>
+</body>
+</html>
