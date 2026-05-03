0567fb4add2841438dc973013b026425b2dec1c8
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2026-05-02 22:23:35 -0300

    alterando config da conexao com database

--- config/database.php
+++ config/database.php
@@ -2,10 +2,36 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+use Dotenv\Dotenv;
 
-$host = $_ENV['DB_HOST'];
-$database = $_ENV['DB_NAME'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
+class Database {
+
+    private static $pdo;
+
+    public static function conectar() {
+
+        if (!self::$pdo) {
+
+            // carregar .env
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+            $dotenv->load();
+
+            try {
+                self::$pdo = new \PDO(
+                    "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4",
+                    $_ENV['DB_USER'],
+                    $_ENV['DB_PASS']
+                );
+
+                self::$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                self::$pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+                self::$pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+
+            } catch (\PDOException $e) {
+                die("Erro na conexão: " . $e->getMessage());
+            }
+        }
+
+        return self::$pdo;
+    }
+}
